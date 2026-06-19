346d6e46cb42ee6f7d843bc5185739312d1fd875
Pandurangan T <td.pandurangan07@gmail.com>
2026-06-19 22:57:45 +0530

    Time: 9 ms (74.3%), Space: 19.6 MB (76.47%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -10,7 +10,6 @@ class Solution:
             i-=1
             k//=10
             n-=1
-            print(a)
         if k==0:
             while n>0:
                 q=a[i]+r
@@ -18,16 +17,13 @@ class Solution:
                 a[i]=q%10 
                 i-=1
                 n-=1
-                print(a)
         else:
             while k>0:
                 q=k%10+r
                 r=q//10
                 a.insert(0,q%10)
                 k//=10
-                print(a)
-    
+            
         if r>0:
             a.insert(0,r)
-            print(a)
         return a
