fac51780c6037fcd57c83c6314e11c8eda7779b4
Pandurangan T <td.pandurangan07@gmail.com>
2026-06-19 19:12:30 +0530

    Time: 4 ms (26.27%), Space: 19.3 MB (84.77%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,19 @@
+import re
+class Solution:
+    def mostCommonWord(self, p: str, banned: List[str]) -> str:
+        s = re.sub(r"[!?',;.]", " ", p)
+        s=s.lower().split()
+        print(s)
+        d={}
+        for w in s:
+            if w not in d:
+                d[w]=1
+            else:
+                d[w]+=1
+        d = dict(sorted(d.items(), key=lambda x: x[1],reverse=True))
+        print(d)
+        for i,j in d.items():
+            if i in banned:
+                continue
+            else:
+                return i
