e815e82e460f418d887c5e61b1de3b6d1f442af1
Pandurangan T <td.pandurangan07@gmail.com>
2026-06-20 14:25:56 +0530

    Delete 0905-sort-array-by-parity directory

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def sortArrayByParity(self, a: List[int]) -> List[int]:
-        e,o=[],[]
-        for i in range(len(a)):
-            if a[i]%2==0:
-                e.append(a[i])
-            else:
-                o.append(a[i])
-        return e+o
