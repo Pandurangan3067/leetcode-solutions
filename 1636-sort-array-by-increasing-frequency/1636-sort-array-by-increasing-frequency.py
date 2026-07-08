fa36752e7e6ca5bd1cef8ae895518dd871efeb6d
Pandurangan T <td.pandurangan07@gmail.com>
2026-07-09 00:03:30 +0530

    Time: 2 ms (69.48%), Space: 19.3 MB (76.51%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,10 +1,14 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        mp={}
-        for i in range(len(nums)):
-            if nums[i] in mp:
-                mp[nums[i]]+=1
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
             else:
-                mp[nums[i]]=1
-        nums.sort(key=lambda x:(mp[x],-x))
-        return nums
+                d[i]=1
+        s=d.items()
+        s = sorted(s, key=lambda x: (x[1], -x[0]))
+        l=[]
+        for i in s:
+            l+=[i[0]]*i[1]
+        return l
