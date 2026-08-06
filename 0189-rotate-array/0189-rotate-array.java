d7f46649cf36ee86a37f28915c460931c1820c63
Pandurangan T <td.pandurangan07@gmail.com>
2026-08-06 17:46:01 +0530

    Time: 2 ms (18.9%), Space: 61.1 MB (85.61%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,4 +1,13 @@
 class Solution {
+
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k = k % n; 
+        if (k == 0) return;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
     public void reverse(int[] nums, int start, int end){
         while (start < end) {
             int temp = nums[start];
@@ -7,14 +16,6 @@ class Solution {
             start++;
             end--;
         }
-    }
-    public void rotate(int[] nums, int k) {
-        int n=nums.length;
-        k = k % n; 
-        if (k == 0) return;
-        reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
     }
     /* 
         * MATH THEORY: Think of the array as two blocks, A (first n-k elements) and B (last k elements). We start with [A, B] and want [B, A].
