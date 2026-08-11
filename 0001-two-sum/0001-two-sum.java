3edd2d785db63b527523f95844bdc0eb7c748f3e
Pandurangan T <td.pandurangan07@gmail.com>
2026-08-11 17:30:17 +0530

    Time: 8 ms (46.79%), Space: 46.8 MB (90.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,33 @@
 class Solution {
     public int[] twoSum(int[] arr, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            int complement = target - arr[i];
-            
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
-            }
-           
-            map.put(arr[i], i);
+        int n = arr.length;
+        int[][] numsWithIndex = new int[n][2];
+        
+        // Store element with original index
+        for (int i = 0; i < n; i++) {
+            numsWithIndex[i][0] = arr[i];
+            numsWithIndex[i][1] = i;
         }
-        return new int[] { -1, -1 };
+        
+        // Sort by the value to apply two-pointer
+        Arrays.sort(numsWithIndex, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = numsWithIndex[left][0] + numsWithIndex[right][0];
+            if (sum == target) {
+                // Return original indices of the two numbers found
+                return new int[] {numsWithIndex[left][1], numsWithIndex[right][1]};
+            } else if (sum < target) {
+                // Increase sum by moving left pointer forward
+                left++;
+            } else {
+                // Decrease sum by moving right pointer backward
+                right--;
+            }
+        }
+        
+        // No pair found
+        return new int[] {-1, -1};
     }
 }
