d3b0571185a6a122be06bb3a18720e31817defdf
Pandurangan T <td.pandurangan07@gmail.com>
2026-08-11 17:26:43 +0530

    Time: 3 ms (58.91%), Space: 47 MB (74.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,14 +3,13 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             int complement = target - arr[i];
-            // If complement found, return indices
+            
             if (map.containsKey(complement)) {
                 return new int[] { map.get(complement), i };
             }
-            // Store current element and index
+           
             map.put(arr[i], i);
         }
-        // No pair found
         return new int[] { -1, -1 };
     }
 }
